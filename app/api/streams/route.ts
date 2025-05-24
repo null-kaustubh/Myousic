fa85d58a567f547d2daae5713bb9f16c93ec812b
Kaustubh Sankhe <kaustubhs2903@gmail.com>
2025-05-24 18:39:07 +0530

    create stream, upvote and downvote routes

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,5 +1,82 @@
-import { NextRequest } from "next/server";
+import { prismaClient } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+const YT_REGEX =
+  /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+
+const CreateStreamSchema = z.object({
+  creatorId: z.string(),
+  url: z.string(),
+});
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
+  try {
+    const data = CreateStreamSchema.parse(await req.json());
+    const isYoutube = data.url.match(YT_REGEX);
+    if (!isYoutube) {
+      return NextResponse.json(
+        {
+          message: `Wrong URL format`,
+        },
+        {
+          status: 411,
+        }
+      );
+    }
+
+    const extractedId = data.url.split("?v=")[1];
+
+    const res = await youtubesearchapi.GetVideoDetails(extractedId);
+    const thumbnails = res.thumbnail.thumbnails;
+    thumbnails.sort((a: { width: number }, b: { width: number }) =>
+      a.width < b.width ? -1 : 1
+    );
+
+    const stream = await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type: "Youtube",
+        title: res.title ?? "Can't find video",
+        smallImageUrl:
+          (thumbnails.length > 1
+            ? thumbnails[thumbnails.length - 2].url
+            : thumbnails[thumbnails.length - 1].url) ??
+          "https://www.powertrac-bd.com/public/backend/assets/images/projects/default.jpg",
+        bigImageUrl:
+          thumbnails[thumbnails.length - 1].url ??
+          "https://www.powertrac-bd.com/public/backend/assets/images/projects/default.jpg",
+      },
+    });
+
+    return NextResponse.json({
+      message: "Added stream",
+      id: stream.id,
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: `Error while adding a stream ${e}`,
+      },
+      {
+        status: 411,
+      }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const creatorId = req.nextUrl.searchParams.get("creatorId");
+  const streams = await prismaClient.stream.findMany({
+    where: {
+      userId: creatorId ?? "",
+    },
+  });
+
+  return NextResponse.json({
+    streams,
+  });
 }
